2314c46952423a7eb2ebe3d05be55e2775472e26
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-08 13:53:49 +0300

    added all the pages and components

--- apple-clone/src/App.js
+++ apple-clone/src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import { useEffect } from "react";
+import MobileSlider from "./component/MobileSlider";
+import YoutubeFunctionality from "./component/YoutubeFunctionality";
+import { Route, Routes } from "react-router-dom";
+import Notfoundpage from "./component/Notfoundpage";
+import Main from "./component/main/Main";
+import Cart from "./pages/cart/Cart";
+import Productpage from "./pages/productpage/Productpage";
+import Iphone from "./pages/iphone/Iphone";
+import Mac from "./pages/mac/Mac";
+import SharedElement from "./component/SharedElement";
 
 function App() {
+  useEffect(() => {
+    document.title = "Apple replica";
+  }, []);
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedElement />}>
+          <Route index element={<Main />} />
+          <Route path="iphone" element={<Iphone />} />
+          <Route path="iphone/:lastUrl" element={<Productpage />} />
+          <Route path="mac" element={<Mac />} />
+          {/* <Route path="search" element={<Search />} /> */}
+          <Route path="cart" element={<Cart />} />
+          <Route path="*" element={<Notfoundpage />} />
+        </Route>
+      </Routes>
+      {/* <MobileSlider /> */}
+    </>
   );
 }
 
--- apple-clone/src/index.js
+++ apple-clone/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './asset/css/fontawesome.css'
+import './asset/css/solid.css'
+import './asset/bootstrap.css'
+import './asset/style.css'
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
